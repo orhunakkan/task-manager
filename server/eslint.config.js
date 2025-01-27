e3ad6a48628c6273e471ad942f3641b836467290
Orhun Akkan <orhun.akkan@aspirion.com>
2025-01-27 03:00:09 -0600

    Add TypeScript and ESLint configuration for client and server

---
+++ server/eslint.config.js
@@ -0,0 +1,17 @@
+const { ESLint } = require("eslint");
+
+module.exports = new ESLint({
+  baseConfig: {
+    parser: "@typescript-eslint/parser",
+    env: {
+      node: true,
+      es2021: true,
+    },
+    extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+    parserOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    rules: {},
+  },
+});
