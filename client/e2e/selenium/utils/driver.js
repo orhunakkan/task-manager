30dcd2d1e2daa00c6b69898f812be7a5760e0c84
orhunakkan <orhunakkan@outlook.com>
2025-02-01 05:42:29 -0600

    Update import path for configuration in Selenium driver utility

--- client/e2e/selenium/utils/driver.js
+++ client/e2e/selenium/utils/driver.js
@@ -1,6 +1,6 @@
 import { Builder } from 'selenium-webdriver';
 import { Options } from 'selenium-webdriver/chrome';
-import { browser, windowSize, implicitTimeout } from '../config/config';
+import { browser, windowSize, implicitTimeout } from '../selenium.config';
 
 async function createDriver() {
   const options = new Options();
