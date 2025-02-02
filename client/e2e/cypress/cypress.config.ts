39c481f9c01f60b7ade5514bfa766c6c3c1f3d94
orhunakkan <orhunakkan@outlook.com>
2025-02-01 23:35:24 -0600

    Refactor Cypress configuration to simplify event setup and add type reference in sample UI test

--- client/e2e/cypress/cypress.config.ts
+++ client/e2e/cypress/cypress.config.ts
@@ -2,9 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    setupNodeEvents() { },
     specPattern: 'e2e/cypress/tests/**/*.cy.{js,jsx,ts,tsx}',
     fixturesFolder: 'e2e/cypress/payloads',
     supportFile: false,
--- client/e2e/cypress/tests/sampleUI.cy.ts
+++ client/e2e/cypress/tests/sampleUI.cy.ts
@@ -1,3 +1,5 @@
+/// <reference types="cypress" />
+
 describe('UI Tests for Homepage', () => {
     beforeEach(() => {
         cy.visit('/');
