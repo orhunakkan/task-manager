510ceb4668ea261777790fbcb672c864e2fb76f0
orhunakkan <orhunakkan@outlook.com>
2025-02-02 01:32:07 -0600

    Remove Selenium and Cypress configuration files along with associated test cases

--- client/e2e/selenium/selenium.config.js
+++
@@ -1,5 +0,0 @@
-export const baseUrl = 'http://localhost:3000';
-export const browser = 'chrome';
-export const implicitTimeout = 5000;
-export const explicitTimeout = 10000;
-export const windowSize = { width: 1920, height: 1080 };
