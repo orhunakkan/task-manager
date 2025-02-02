321f7901e784c997325a0dcbdf625e4fe77bbf16
orhunakkan <orhunakkan@outlook.com>
2025-02-02 00:20:23 -0600

    Add Allure reporting support for Vitest and update package dependencies

--- client/vitest.config.ts
+++ client/vitest.config.ts
@@ -1,15 +1,22 @@
 /// <reference types="vitest" />
+import AllureReporter from "allure-vitest/reporter";
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
     globals: true,
-    include: [
-      'src/**/*.{test,spec}.{js,ts,jsx,tsx}',
-      // 'e2e/selenium/tests/**/*.test.{js,ts}'
-    ],
+    include: ['src/**/*.{test,spec}.{js,ts,jsx,tsx}'],
     exclude: ['**/node_modules/**', '**/dist/**', '**/coverage/**'],
     environment: 'jsdom',
-    setupFiles: './src/test/setup.ts',
+    setupFiles: ['./src/test/setup.ts', 'allure-vitest/setup'],
+    reporters: [
+      "verbose",
+      [
+        "allure-vitest/reporter",
+        {
+          resultsDir: "allure-results",
+        },
+      ],
+    ],
   }
 });
