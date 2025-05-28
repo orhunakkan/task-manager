0e70f65e38e3e3074d885597bad65f36da794b12
orhunakkan <orhunakkan@outlook.com>
2025-05-28 13:35:53 -0500

    favicon, submit button added

--- script.js
+++ script.js
@@ -3,3 +3,11 @@
 document.querySelector('#create-task-button').addEventListener('click', function () {
 	document.querySelector('#task-form').style.display = 'block';
 })
+
+document.querySelector('#close-btn').addEventListener('click', function () {
+  	document.querySelector('#task-form').style.display = 'none';
+});
+
+document.querySelector('#task-form').addEventListener('submit', function (e) {
+  e.preventDefault();
+});
